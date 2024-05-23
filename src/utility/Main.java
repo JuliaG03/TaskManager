9f4cd2368f88a277e5eb96d5d2d0f93ac0f04697
JuliaG03 <julia.grasu@s.unibuc.ro>
2024-05-23 13:56:27 +0300

    done csv audit service

--- src/utility/Main.java
+++ src/utility/Main.java
@@ -2,6 +2,7 @@ package utility;
 
 import DataBase.JdbcSettings;
 import model.User;
+import services.AuditService;
 import services.AuthenticationService;
 import services.MainService;
 
@@ -15,74 +16,20 @@ public class Main {
 
     public Main() {}
 
-    public static void main(String[] args) throws ParseException{
+    public static void main(String[] args) throws ParseException {
         List<User> listOfUsers = new ArrayList<>();
         User loggedinuser = null;
-        Data data = new Data(listOfUsers,loggedinuser);
+        Data data = new Data(listOfUsers, loggedinuser);
         AuthenticationService authenticationService = new AuthenticationService(data);
-        MainService mainService = new MainService(data) ;
+        MainService mainService = new MainService(data);
         JdbcSettings J = JdbcSettings.getJdbcSettings();
+        AuditService.getInstance().logAction("openingProject");
 
         mainService.startapp();
 
         mainService.authentication();  // + methods for admin
 
 
-
-
-
-
-        data.getLoggedin().printUserDetails();
-
-
-
     }
+}
 
-
-
-
-    /*
-    public static void main(String[] args) throws ParseException {
-        Scanner in = new Scanner(System.in);
-        boolean end = false;
-         model.User loggedinUser = null;
-         services.MainService.java mainService = new services.MainService.java();
-       while(!end){
-
-           System.out.println("Welcome to model.Task manager!");
-           System.out.println("\n What are you ? \n 1. model.User \n 2. Admin\n (Enter 1 or 2)\n");
-           String typeofUser = in.nextLine();
-           System.out.println(typeofUser + "user type \n");
-           if (Objects.equals(typeofUser, "1")){  //admin
-               mainService.logIn(in);
-               System.out.println("\nWhat do you want to do?\n");
-               System.out.println("1. See all users \n2.Delete a user\n 3.Exit");
-               String choice = in.nextLine();
-                       if(Objects.equals(choice, "1")) mainService.seeAllUsers();
-                       else if(choice == "2"){
-                           //to write function that has the admin choose a user by index:
-                           //model.User usertoDelete = mainService.chooseUser();
-                          // mainService.deleteUser(userToDelete);
-                       }
-                       else {end = true;}
-           }
-           else if (Objects.equals(typeofUser, "2")){ //user
-           System.out.println("\nWhat do you want to do today? ");
-           System.out.println(" 1. Register \n" +
-                                " 2. Login\n"+
-                                " 3. ");}
-
-           else {
-               System.out.println("Invalid command.");
-               end = true;}
-
-
-
-       }
-
-        }
-
-*/
-
-
-    }
